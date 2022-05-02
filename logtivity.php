cf450816d981b397ff75926e1ac98bcca8137bf7
Ralph Morris <ralph@10degrees.uk>
2022-05-02 19:09:28 +0100

    update version

--- logtivity.php
+++ logtivity.php
@@ -4,14 +4,14 @@
  * Plugin Name: Logtivity
  * Plugin URI:  https://logtivity.io
  * Description: Dedicated Event Monitoring for WordPress using Logtivity.io.
- * Version:     1.15.0
+ * Version:     1.16.0
  * Author:      Logtivity
  * Text Domain: logtivity
  */
 
 class Logtivity
 {
-	protected $version = '1.15.0';
+	protected $version = '1.16.0';
 
 	/**
 	 * List all classes here with their file paths. Keep class names the same as filenames.
